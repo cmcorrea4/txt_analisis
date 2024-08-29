f56879e3d133e99dfa84b6a59e9358f011b435da
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-28 19:39:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ translator = Translator()
 
 
 text = st.text_area('Escribe por favor: ')
-if text is None:
+if text:
    #translation = translator.translate(text, src="es", dest="en")
    #trans_text = translation.text
     opcion_elegida = st.selectbox(
@@ -20,23 +20,23 @@ if text is None:
         ("Oraciones en el párrafo", "Sinónimo", "Definición"),
     )
     
-# Ejecutar código según la opción seleccionada
-if opcion_elegida == "Oraciones en el párrafo":
-  t=TextBlob(text)
-  st.write("Has elegido la Opción 1. Aquí va el código específico para esta opción:")
-  # Código para la Opción 1
-  st.write("print('Hola desde la Opción 1')")
-  print("Tenemos", len(t.sentences), "oraciones.\n")
-  for sentence in t.sentences:
-      st.write(sentence)
-      st.write("-" * 75)
-
-elif opcion_elegida == "Sinónimo":
-  st.write("Has elegido la Opción 2. Aquí va el código específico para esta opción:")
-  # Código para la Opción 2
-  st.write("Hola desde la Opción 2")
-else:
-  st.write("Has elegido la Opción 3. Aquí va el código específico para esta opción:")
-  # Código para la Opción 3
-  st.write("Hola desde la Opción")
+   # Ejecutar código según la opción seleccionada
+   if opcion_elegida == "Oraciones en el párrafo":
+     t=TextBlob(text)
+     st.write("Has elegido la Opción 1. Aquí va el código específico para esta opción:")
+     # Código para la Opción 1
+     st.write("print('Hola desde la Opción 1')")
+     print("Tenemos", len(t.sentences), "oraciones.\n")
+     for sentence in t.sentences:
+         st.write(sentence)
+         st.write("-" * 75)
+   
+   elif opcion_elegida == "Sinónimo":
+     st.write("Has elegido la Opción 2. Aquí va el código específico para esta opción:")
+     # Código para la Opción 2
+     st.write("Hola desde la Opción 2")
+   else:
+     st.write("Has elegido la Opción 3. Aquí va el código específico para esta opción:")
+     # Código para la Opción 3
+     st.write("Hola desde la Opción")
 
