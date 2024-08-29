3eef0b9cd4d3ed5f8bd5e8345629f076118fd057
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-28 20:56:05 -0500

    Update corpora.py

--- corpora.py
+++ corpora.py
@@ -1,4 +1,13 @@
 import subprocess
-import textblob 
-cmd = ['python3','-m','textblob.download_corpora']
-subprocess.run(cmd)
+#cmd = ['python3','-m','textblob.download_corpora']
+#subprocess.run(cmd)
+def descargar_corpora():
+    try:
+        # Ejecutar el comando "python -m textblob.download_corpora"
+        subprocess.run(["python", "-m", "textblob.download_corpora"], check=True)
+        print("Corpora descargados correctamente.")
+    except subprocess.CalledProcessError:
+        print("Error al descargar los corpora.")
+
+if __name__ == "__main__":
+    descargar_corpora()
