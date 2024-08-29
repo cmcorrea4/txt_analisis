6a49db7c5975662311cf2d80f6b54174efd48078
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-28 19:40:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -20,23 +20,23 @@ if text:
         ("Oraciones en el párrafo", "Sinónimo", "Definición"),
     )
     
-   # Ejecutar código según la opción seleccionada
-   if opcion_elegida == "Oraciones en el párrafo":
-     t=TextBlob(text)
-     st.write("Has elegido la Opción 1. Aquí va el código específico para esta opción:")
-     # Código para la Opción 1
-     st.write("print('Hola desde la Opción 1')")
-     print("Tenemos", len(t.sentences), "oraciones.\n")
-     for sentence in t.sentences:
+   
+    if opcion_elegida == "Oraciones en el párrafo":
+       t=TextBlob(text)
+       st.write("Has elegido la Opción 1. Aquí va el código específico para esta opción:")
+       # Código para la Opción 1
+       st.write("print('Hola desde la Opción 1')")
+       print("Tenemos", len(t.sentences), "oraciones.\n")
+       for sentence in t.sentences:
          st.write(sentence)
          st.write("-" * 75)
    
-   elif opcion_elegida == "Sinónimo":
-     st.write("Has elegido la Opción 2. Aquí va el código específico para esta opción:")
-     # Código para la Opción 2
-     st.write("Hola desde la Opción 2")
-   else:
-     st.write("Has elegido la Opción 3. Aquí va el código específico para esta opción:")
-     # Código para la Opción 3
-     st.write("Hola desde la Opción")
+     elif opcion_elegida == "Sinónimo":
+       st.write("Has elegido la Opción 2. Aquí va el código específico para esta opción:")
+       # Código para la Opción 2
+       st.write("Hola desde la Opción 2")
+     else:
+       st.write("Has elegido la Opción 3. Aquí va el código específico para esta opción:")
+       # Código para la Opción 3
+       st.write("Hola desde la Opción")
 
